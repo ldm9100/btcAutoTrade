500d0778d78c57d475b3dd9a1c3376ec26efbe34
ldm9100 <ldm9100@naver.com>
2023-02-23 11:57:12 +0900

    for stx

--- bat.py
+++ bat.py
@@ -40,20 +40,20 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-STX")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            current_price = get_current_price("KRW-BTC")
+            target_price = get_target_price("KRW-STX", 0.5)
+            current_price = get_current_price("KRW-STX")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-STX", krw*0.9995)
         else:
-            btc = get_balance("BTC")
-            if btc > 0.0002:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+            btc = get_balance("KRW-STX")
+            if btc > 5:
+                upbit.sell_market_order("KRW-STX", btc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
